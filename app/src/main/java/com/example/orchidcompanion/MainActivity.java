c7aeee601473bce8e3ed5da5f797f3ff8dee96fb
Denis Onder <denis_onder@protonmail.com>
2021-04-18 23:20:39 +0200

    Getting the button, and registering events

--- app/src/main/java/com/example/orchidcompanion/MainActivity.java
+++ app/src/main/java/com/example/orchidcompanion/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.orchidcompanion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.main_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
